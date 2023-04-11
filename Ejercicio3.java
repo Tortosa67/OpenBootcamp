c826988b472e8c78079caf1570aaafe1cb0a0c6c
Tortosa67 <123087044+Tortosa67@users.noreply.github.com>
2023-04-11 08:19:16 +0200

    Create Ejercicio3.java

---
+++ Ejercicio3.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        String[] nombres = {"Ana", "Lucia", "Nuria", "Noelia"};
+         for( String nombre: nombres){
+             System.out.println(nombre);
+
+         }
+    }
+}
